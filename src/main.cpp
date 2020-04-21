28154d3cdced2fc517461f0ced44ba0f9000b127
Alban Ponche <alban@P64LIXBICH4>
2020-04-21 10:03:07 +0200

    Add GPLv3 and README

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,38 @@
+/*
+    This project is for convertir PWM Signal from an RC Remote controll receiver (Ex : FRSKY X8R)
+    wich is coded in PWM with a duty cycle of 10% (1->2ms value range over  18ms), to PWM signal
+    with a duty cycle range of 100% (0->100%)
+
+    The main usage is for replacing RF receiver, from a basic RC CAR, with a FRSKY module, 
+    to control the car with a TARANIS remote control.
+
+    With a ESP8266, gave me a 4 channels converter (4 in/4 out)
+    
+    Copyright (C) 2020 Alban Ponche
+    https://github.com/warrenberberd/PWMConverter
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+
+
 #include <Arduino.h>
 
 void setup() {
-  // put your setup code here, to run once:
+  
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  
 }
